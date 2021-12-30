869b500d7f4e4bf1209645a0b0e2f655c957ae58
Matthew Oates <matthew-oates@outlook.com>
2021-12-30 00:04:33 +0000

    bug fix for amplify hosting

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import App from './App';
 import './styles/index.css';
 import {ThemeProvider} from '@mui/material/styles';
 import {themeOptions} from './styles/theme';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 
 ReactDOM.render(
   <ThemeProvider theme={themeOptions}>
